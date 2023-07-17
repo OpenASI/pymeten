2d65c39cf4234fbb8d0f12e620110226359799e9
deciding <zhangzn710@gmail.com>
2023-07-17 12:20:30 +0000

    v0.0.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'A tool for metrics of tensors'
 LONG_DESCRIPTION = 'Measure the different aspects of your tensor program efficiency'
 
